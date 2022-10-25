a5f79c709017dfea497d76c6839e71ef20858a79
wtrknght <116600901+wtrknght@users.noreply.github.com>
2022-10-24 20:06:05 -0400

    Update retry.py

--- retry.py
+++ retry.py
@@ -5,9 +5,9 @@ technobladequote = random.choice(quotes.quoter)
 def printquote():
     ggg = len(technobladequote)
     if ggg >= 30:
-        iflong = 18
-    else:
         iflong = 6
+    else:
+        iflong = 18
     label = Tk.Label(None, text=technobladequote, font=('Times', iflong),fg='black')
     label.pack()
     label.mainloop()
